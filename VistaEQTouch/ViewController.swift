43bf0e38dfd71127285e0c805d6b197272e5b416
María Antonia Tapia <Maria@Rafa-MacBook.local>
2018-03-09 09:34:06 -0300

    Initial Commit

---
+++ VistaEQTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VistaEQTouch
+//
+//  Created by María Antonia Tapia on 9/3/18.
+//  Copyright © 2018 examples. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
